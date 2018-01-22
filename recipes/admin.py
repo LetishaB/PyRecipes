5098eb4461ece855f167ad74c62bbf5104a8bb89
Leticia <LetishaB@users.noreply.github.com>
2018-01-22 16:28:32 +0100

    admin and some model changes

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
 # Register your models here.
+from recipes.models import Recipe, Direction, Ingredient
+
+
+@admin.register(Ingredient)
+class IngredientsAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    fields = ('name',)
+
+
+@admin.register(Direction)
+class DirectionAdmin(admin.ModelAdmin):
+    list_display = ('step',)
+    fields = ('step',)
+
+
+class IngredientsInline(admin.TabularInline):
+    model = Recipe.ingredients.through
+    extra = 1
+
+
+class DirectionsInline(admin.TabularInline):
+    model = Recipe.directions.through
+    extra = 1
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    fields = ('title', 'created_by', 'image', 'description',)
+    list_display = ('title', 'created_by', 'created_at')
+    exclude = ('created_at',)
+    inlines = [
+        IngredientsInline,
+        DirectionsInline,
+    ]
--- recipes/models.py
+++ recipes/models.py
@@ -6,16 +6,25 @@ from django.db import models
 class Ingredient(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 
 class Direction(models.Model):
     step = models.TextField()
 
+    def __str__(self):
+        return self.step
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=50)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     image = models.ImageField(upload_to='static/images/', default='static/images/missing.png')
     ingredients = models.ManyToManyField(Ingredient)
     directions = models.ManyToManyField(Direction)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
